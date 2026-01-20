71d4ddb3abd62a09e7862a8fbe4f8142a9414839
YT-MrXT <canalmrxt@gmail.com>
2026-01-20 21:17:13 +0000

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -4,4 +4,4 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Icon = "rbxthumb://type=Asset&id=96866982801235&w=150&h=150";
     Duration = 5;
 })
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/DevHub-roblox/script-SYNTRAX-Hub/refs/heads/main/Game/Bloxfruit/SYNTRAXHub/main.txt.lua"))()

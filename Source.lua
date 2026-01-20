32b4cd746f52a067b971fa90def27934be2d94a8
YT-MrXT <canalmrxt@gmail.com>
2026-01-20 17:12:58 +0000

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -4,4 +4,4 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Icon = "rbxthumb://type=Asset&id=96866982801235&w=150&h=150";
     Duration = 5;
 })
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
